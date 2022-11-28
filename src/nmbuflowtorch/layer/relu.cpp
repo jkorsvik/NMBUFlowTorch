103de295fa842ee2fb3e2cedc8e9d01d142988b7
Jon-Mikkel Korsvik <jkorsvik@outlook.com>
2022-11-28 21:12:53 +0100

    removing uneeded comments

--- src/nmbuflowtorch/layer/relu.cpp
+++ src/nmbuflowtorch/layer/relu.cpp
@@ -5,15 +5,12 @@ namespace nmbuflowtorch::layer
 
   void ReLU::forward(const Matrix& X)
   {
-    // a = z*(z>0)
-    layer_input = X;
+      layer_input = X;
     layer_output = X.cwiseMax(0.0);
   }
 
   void ReLU::backward(const Matrix& X, const Matrix& accumulated_gradients)
   {
-    // d(L)/d(z_i) = d(L)/d(a_i) * d(a_i)/d(z_i)
-    //             = d(L)/d(a_i) * 1*(z_i>0)
     Matrix positive = (X.array() > 0.0).cast<float>();
     gradient_back = accumulated_gradients.cwiseProduct(positive);
   }

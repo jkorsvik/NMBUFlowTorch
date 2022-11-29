f987119ae35442dea2053e779aadf02a027c8f7f
Jon-Mikkel Korsvik <jkorsvik@outlook.com>
2022-11-29 03:01:19 +0100

    epoch and fit, printing if verbose

--- include/nmbuflowtorch/definitions.hpp
+++ include/nmbuflowtorch/definitions.hpp
@@ -1,7 +1,11 @@
 #ifndef NMBUFLOWTORCH_DEFINITIONS_H_
 #define NMBUFLOWTORCH_DEFINITIONS_H_
 
+#include <unistd.h>  // Unix things for sleep function in our case
+
 #include <Eigen/Dense>
+#include <cmath>
+#include <cstdio>
 #include <iostream>
 #include <string>
 
@@ -9,9 +13,30 @@ typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> Matrix;
 typedef Eigen::Matrix<float, Eigen::Dynamic, 1> Vector;
 typedef Eigen::Array<float, 1, Eigen::Dynamic> RowVector;
 
+#define PBWIDTH              5
+#define PERCENTAGEMULTIPLIER 100
+#define PBSTR                '\xFE'  // "█"
+
 namespace nmbuflowtorch
 {
+  inline void printProgress(float current, float total, int verbose_level)
+  {
+    // int val = (int)(percentage * PERCENTAGEMULTIPLIER);
+    // int lpad = (int)(percentage * PBWIDTH);
+    // int rpad = PBWIDTH - lpad;
+    // printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
 
+    float percent = (PERCENTAGEMULTIPLIER * (current)) / total;
+    percent = percent > PERCENTAGEMULTIPLIER ? PERCENTAGEMULTIPLIER : percent;  // Prevents percentage from going over 100%
+    size_t lpad = (size_t)(percent / PBWIDTH);
+    size_t rpad = (size_t)(PERCENTAGEMULTIPLIER / PBWIDTH - lpad);
+
+    std::wcout << "\r"
+               << "[" << std::wstring(lpad, PBSTR) << std::wstring(rpad, ' ') << "]";
+    std::cout << percent << "%"
+              << " [Batch " << current << " of " << total << "]";
+    std::cout.flush();
+  }
 };  // namespace nmbuflowtorch
 
 #endif  // NMBUFLOWTORCH_DEFINITIONS_H_

2b95c5c6142d7b6bdaf357641fe8659afcadceef
Jon-Mikkel Korsvik <jkorsvik@outlook.com>
2022-12-04 00:35:22 +0100

    cmake doxygen

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -5,6 +5,7 @@ if(${PROJECT_NAME}_ENABLE_DOXYGEN)
     set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
 
     find_package(Doxygen REQUIRED)
+    find_package(Doxygen OPTIONAL_COMPONENTS dot)
     doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
 
     verbose_message("Doxygen has been setup and documentation is now available.")

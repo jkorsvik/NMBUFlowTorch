a830c570c2ea2acc985faf3090d766113ae3aa7b
Jon-Mikkel Korsvik <jkorsvik@outlook.com>
2022-12-04 01:26:46 +0100

    amazing documentation

--- .docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['readme_2',['README',['../md_README.html',1,'']]]
-];
--- .docs/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pull_5frequest_5ftemplate_4',['PULL_REQUEST_TEMPLATE',['../md_PULL_REQUEST_TEMPLATE.html',1,'']]]
-];
